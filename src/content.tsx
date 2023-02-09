e4a29b66b4be78c23751c3c00acffb0725da1d9c
Jie.Tan <taamjoe@gmail.com>
2023-02-09 22:16:32 +0800

    feat:预售全价

--- src/content.tsx
+++ src/content.tsx
@@ -28,33 +28,39 @@ const authVal = document.cookie
   .map((a) => a.split("="))
   .find(([name]) => name.trim() === "cookie_user_auth") as any;
 
-const param = {
-  number: 1,
-  ticketId: "2129109933677813767713971",
-  isElectronic: 1,
-  isExpress: 0,
-  deviceFrom: "wap",
-  actual: 120,
-  performanceId: "2129101812876124162473378",
-  timeId: "2129102470501048321800919",
-  returnUrl: "https://m.zhengzai.tv/pay/status?order_type=ticket&order_id=",
-  showUrl: "https://m.zhengzai.tv/pay/status?order_type=ticket&order_id=",
-  expressType: 0,
-  agentId: 0,
-  payType: "alipay",
-};
-const n = CryptoJs.AES.encrypt(
-  JSON.stringify(param),
-  CryptoJs.enc.Base64.parse("XjjkaLnlzAFbR399IP4kdQ=="),
-  {
-    mode: CryptoJs.mode.ECB,
-    padding: CryptoJs.pad.Pkcs7,
-    length: 128,
-  }
-).toString();
-const i = new Date().getTime();
-console.log({ n });
-setTimeout(() => {
+const postData = (data: {
+  ticketId: string;
+  performanceId: string;
+  timeId: string;
+}) => {
+  const param = {
+    number: 1,
+    // ticketId: "2147175463454187521441808",
+    // performanceId: "2147175463244472326515578",
+    // timeId: "2147175463328358409303173",
+    isElectronic: 1,
+    isExpress: 0,
+    deviceFrom: "wap",
+    actual: 220,
+    returnUrl: "https://m.zhengzai.tv/pay/status?order_type=ticket&order_id=",
+    showUrl: "https://m.zhengzai.tv/pay/status?order_type=ticket&order_id=",
+    expressType: 0,
+    agentId: 0,
+    payType: "alipay",
+    ...data,
+  };
+
+  const n = CryptoJs.AES.encrypt(
+    JSON.stringify(param),
+    CryptoJs.enc.Base64.parse("XjjkaLnlzAFbR399IP4kdQ=="),
+    {
+      mode: CryptoJs.mode.ECB,
+      padding: CryptoJs.pad.Pkcs7,
+      length: 128,
+    }
+  ).toString();
+  const i = new Date().getTime();
+
   fetch("https://order.zhengzai.tv/order/order/pre", {
     method: "post",
     headers: {
@@ -71,36 +77,42 @@ setTimeout(() => {
     }),
   })
     .then((res) => res.json())
-    .then(console.log);
-}, 2000);
+    .then((res) => {
+      if (res.message) {
+        console.log(`%c${res.message}`, `color:red;font-size:60px`);
+      }
+    });
+};
 
-// XjjkaLnlzAFbR399IP4kdQ==
+const tickets = [
+  {
+    ticketId: "2147175463454187521441808",
+    performanceId: "2147175463244472326515578",
+    timeId: "2147175463328358409303173",
+  },
+  {
+    ticketId: "2147175463538073603705071",
+    performanceId: "2147175463244472326515578",
+    timeId: "2147175463328358409303173",
+  },
+  {
+    ticketId: "2147174625893621763189765",
+    performanceId: "2147174625683906568235086",
+    timeId: "2147174625809735680789387",
+  },
+  {
+    ticketId: "2147174626019450885711453",
+    performanceId: "2147174625683906568235086",
+    timeId: "2147174625809735680789387",
+  },
+];
 
-/*
-v = {
-            encryptByAES: function(t) {
-                var e = A()(t)
-                  , n = m.a.AES.encrypt(e, m.a.enc.Base64.parse("XjjkaLnlzAFbR399IP4kdQ=="), {
-                    mode: m.a.mode.ECB,
-                    padding: m.a.pad.Pkcs7,
-                    length: 128
-                }).toString()
-                  , i = (new Date).getTime();
-                return {
-                    sign: m.a.SHA1(n + i + "QGZUanpSaSy9DEPQFVULJQ==").toString(),
-                    encryptedData: n,
-                    timestamp: i
-                }
-            },
-            decryptByAES: function(t) {
-                var e = m.a.enc.Utf8.parse("TheKeyOfmyDatadx");
-                return m.a.AES.decrypt(t, e, {
-                    mode: m.a.mode.ECB,
-                    padding: m.a.pad.Pkcs7
-                }).toString(m.a.enc.Utf8)
-            }
-        };
- */
-
-// Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxODExMjk5IiwiY19hdCI6IjIwMjEwMzE3MTM0MDUyIiwibW9iaWxlIjoiMTg4Mjc2NjYzMjgiLCJuaWNrbmFtZSI6IjE4OCoqKio2MzI4IiwidHlwZSI6InVzZXIiLCJleHAiOjE2Nzg0NTc2MTQsImlhdCI6MTY3NTg2NTYxNH0.y_T02qaJ67XN3tY5sWLF9gCt7g9kzhdCFPCORDhFBdM
-// Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxODExMjk5IiwiY19hdCI6IjIwMjEwMzE3MTM0MDUyIiwibW9iaWxlIjoiMTg4Mjc2NjYzMjgiLCJuaWNrbmFtZSI6IjE4OCoqKio2MzI4IiwidHlwZSI6InVzZXIiLCJleHAiOjE2Nzg0NTc2MTQsImlhdCI6MTY3NTg2NTYxNH0.y_T02qaJ67XN3tY5sWLF9gCt7g9kzhdCFPCORDhFBdM
+tickets.forEach((item, i) => {
+  if (i === 0) {
+    postData(item);
+  } else {
+    setTimeout(() => {
+      postData(item);
+    }, i * 100);
+  }
+});
